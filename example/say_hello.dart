088f153e912be17588334d2a6fbc8974f67aa9c6
Mochammad Hari Fitrian <mochammadharifitrian@gmail.com>
2025-01-20 09:35:07 +0700

    add belajar-dart-packages-library

---
+++ example/say_hello.dart
@@ -0,0 +1,11 @@
+import 'package:belajar_dart_packages_library/say_hello.dart';
+
+void main(List<String> args) {
+  var say = sayHello("hary");
+  print(say);
+
+  Product product = Product();
+  print(product);
+  Category category = Category();
+  print(category);
+}
---
+++ lib/say_hello.dart
@@ -0,0 +1,6 @@
+library belajar_dart_packages_library;
+
+export 'src/library_say_hello.dart';
+export 'src/library_class_customer.dart' show Product, Category;
+
+
